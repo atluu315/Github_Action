6b0a517ef404764b3730ea4539e19001ef306cb1
atluu315 <61399218+atluu315@users.noreply.github.com>
2020-06-09 02:43:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const {execute} = require('katalon-cli/src/katalon-studio');
 const core = require('@actions/core');
 
-/*const user_ksVersion = core.getInput('theksVersion');
+const user_ksVersion = core.getInput('theksVersion');
 const user_ksLocation = core.getInput('theksLocation');
 const user_ksProjectPath = core.getInput('theksProjectPath');
 const user_ksArgs = core.getInput('theksArgs');
 const user_x11Display = core.getInput('thex11Display');
 const user_xvfbConfiguration = core.getInput('xvfbConfiguration');
-const user_Logger = core.getInput('theLogger');*/
 
 try
 {
     execute(
-        '7.4.0',
-        '',
-        'simple_project/simple_project',
-        '-retry=0 -testSuiteCollectionPath="Test Suites/New Test Suite Collection" -apiKey="fdb89052-b651-46fe-ac7c-b7b4b305eb55"',
-        '',
-        '',
+        user_ksVersion,
+        user_ksLocation,
+        user_ksProjectPath,
+        user_ksArgs,
+        user_x11Display,
+        user_xvfbConfiguration,
         {
             info: function()
             {
