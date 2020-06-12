2a596e611447052787333717f9af08216d211def
atluu315 <61399218+atluu315@users.noreply.github.com>
2020-06-12 01:35:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const {execute} = require('katalon-cli/src/katalon-studio');
 const core = require('@actions/core');
 
 const user_version = core.getInput('version');
-const user_location = core.getInput('location');
 const user_projectPath = core.getInput('projectPath');
 const user_args = core.getInput('args');
 
@@ -10,7 +9,7 @@ try
 {
     execute(
         user_version,
-        user_location,
+        '',
         user_projectPath,
         user_args,
         '',
