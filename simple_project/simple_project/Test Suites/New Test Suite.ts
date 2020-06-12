3cec850ac277a1afb1075233147a6446f1465d89
atluu315 <61399218+atluu315@users.noreply.github.com>
2020-06-12 02:31:16 -0700

    Add files via upload

---
+++ simple_project/simple_project/Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>83b2ddf4-795e-4b19-a239-8cd3383de851</testSuiteGuid>
+   <testCaseLink>
+      <guid>f950c332-c52c-4588-9915-fe2ece111503</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
